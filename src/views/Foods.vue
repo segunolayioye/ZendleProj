d6fca5e143b167508eaf7deb48fe7a47d622d67a
Segun Olayioye <segunolayioye17@gmail.com>
2026-02-13 11:42:05 +0000

    first commit

---
+++ src/views/Foods.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="bg-white w-full md:w-full">
+    <div class="md:px-40 w-full h-[75px] bg-white shadow-md flex items-center justify-between px-5">
+      <div class="flex items-center gap-2">
+        <img src="/src/assets/Grad cap.svg" class="w-[18px]" alt="grad cap">Baze university<img src="/src/assets/down arrow.svg" class="mt-4 w-2.5" alt="arrow">
+      </div>
+      <div class="hidden md:flex justify-start px-2 md:px-10">
+        <button class="bg-gray-100 rounded-full w-[120px] md:w-[300px] h-[28px] pl-5 ">
+          <div class="flex justify-start items-center gap-2 text-gray-500 md:text-xs">
+            <img src="/src/assets/search icon.svg" class="w-3 md:w-5" alt="search icon">Search Zendle</div></button>
+      </div>
+      <div class="flex items-center mr-14 md:flex md:items-center w-[32px] h-[32px] gap-2">
+        <img src="/src/assets/troley.svg" alt="troley">
+        <img src="/src/assets/profile.svg" alt="profile">
+      </div>
+    </div>
+    <div class="mt-7 ml-11 text-xl">All Stores</div>  
+
+    <div class="grid grid-cols-1 px-28 md:grid-cols-4 gap-5 mt-5 md:px-10 mb-11">
+      <div class="w-full">
+        <img src="/src/assets/pizza.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/swallow.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/rice.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/fries.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/rice.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/fries.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/pizza.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/swallow.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/fries.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/rice.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/swallow.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+
+      <div class="w-full">
+        <img src="/src/assets/pizza.svg" class="h-40 w-full object-cover rounded-lg" />
+        <p class="mt-2 font-semibold text-sm">Bukahut</p>
+        <p class="flex items-center gap-2 text-xs text-gray-700"><img src="/src/assets/clock.svg" class="w-5" alt="clock">30-35 mins</p>
+      </div>
+      
+
+    </div>
+ </div> 
+</template>
